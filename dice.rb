0223e1e479029483adf59082099c6bda5a7652b6
Carlos Castaneda <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-07-25 21:26:10 +0000

    add view templates and layout template

--- dice.rb
+++ dice.rb
@@ -8,13 +8,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get('/') do
-  "<h1>Dice Roll</h1>
-    <ul>
-    <li><a href='/dice/2/6'>Roll two 6-sided dice</a></li>
-    <li><a href='/dice/2/10'>Roll two 10-sided dice</a></li>
-    <li><a href='/die/1/20'>Roll one 20-sided die</a></li>
-    <li><a href='/dice/5/4'>Roll five 4-sided die</a></li>
-    </ul>"
+  erb(:home)
 end 
 
 get('/zebra') do
@@ -26,51 +20,61 @@ get('/giraffe') do
 end
 
 get('/dice/2/6') do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..6)
 
-  "<h1>2d6</h1>
-    <p>#{outcome}</p>
-    <a href='/'>Back to homepage</a>"
+    @rolls.push(die)
+  end
+
+  erb(:two_six)
 end
 
 get('/dice/2/10') do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..10)
 
-  "<h1>2d10</h1>
-    <p>#{outcome}</p>
-    <a href='/'>Back to homepage</a>"
+    @rolls.push(die)
+  end
+
+  erb(:two_ten)
 end
 
 get('/die/1/20') do
-  die = rand(1..20)
+  @rolls = []
 
-  outcome = "You rolled a #{die}."
+  1.times do
+    die = rand(1..20)
 
-  "<h1>1d10</h1>
-    <p>#{outcome}</p>
-    <a href='/'>Back to homepage</a>"
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 
 get('/dice/5/4') do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
+  @rolls = []
 
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  5.times do
+    die = rand(1..4)
 
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die} and #{fifth_die} for a total of #{sum}."
+    @rolls.push(die)
+  end
 
-  "<h1>5d4</h1>
-    <p>#{outcome}</p>
-      <a href='/'>Back to homepage</a>"
+  erb(:five_four)
 end
+
+# get('/dice/100/6') do
+#   @rolls = []
+
+#   100.times do
+#     die = rand(1..6)
+
+#     @rolls.push(die)
+#   end
+
+#   erb(:one_hundred_six)
+# end
